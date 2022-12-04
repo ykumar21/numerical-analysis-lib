3b511e2f4f25b838b713a26b39e6bf7531b53147
Yashwardhann Kumar <yash20008@gmail.com>
2022-12-04 19:58:21 +0000

    Formatted Code For Euler method

--- .ccls-cache/@home@runner@Numerical-Analysis-Codes/src@ODE@euler_method.h
+++ .ccls-cache/@home@runner@Numerical-Analysis-Codes/src@ODE@euler_method.h
@@ -17,13 +17,14 @@ public:
 
 template <typename F> void EulerMethod<F>::solve() {
   double prev_x = x0;
-    double prev_y = f(x0);
-  for (int i = 0; i < N; ++i) {
+  double prev_y = f(x0);
+  for (int i = 1; i <= N; ++i) {
     double xi = prev_x + h;
     double yi = prev_y + h * f(prev_x);
     printf("X_%d = %f ", i, xi);
     printf("Y_%d = %f\n", i, yi);
-    prev_x = xi; prev_y = yi;
+    prev_x = xi;
+    prev_y = yi;
   }
 }
 
--- src/ODE/euler_method.cpp
+++ src/ODE/euler_method.cpp
@@ -4,17 +4,16 @@
 
 typedef double (*F)(double);
 
-
 #define DEBUG
 
 double f(double x) { return sqrt(x); }
 
 #ifdef DEBUG
-int main(int argc, char **argv) { 
-    double x0 = 0; 
-    double N = 20; 
-    double h = 0.2;
-    F func_ptr = f;
-    EulerMethod<F> solver(x0,h,N,f); 
+int main(int argc, char **argv) {
+  double x0 = 0;
+  double N = 20;
+  double h = 0.2;
+  F func_ptr = f;
+  EulerMethod<F> solver(x0, h, N, f);
 }
 #endif
